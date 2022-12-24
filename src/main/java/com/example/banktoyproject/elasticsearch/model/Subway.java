5d8a95cf329dd21fd4c920e539f272f59900d1dc
hyojin <wkdgywls30@gmail.com>
2022-12-24 16:03:29 +0900

    elasticSearch for subway

---
+++ src/main/java/com/example/banktoyproject/elasticsearch/model/Subway.java
@@ -0,0 +1,17 @@
+package com.example.banktoyproject.elasticsearch.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class Subway {
+    private String code;
+    private String station;
+    private String chosung;
+    private String jamo;
+    private String engtokor;
+    private String line;
+    private String excode;
+
+}
