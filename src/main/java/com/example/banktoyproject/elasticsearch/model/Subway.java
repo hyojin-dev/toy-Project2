037aa26cf1a060cea04aff8979b3ef4f1d92aa5d
hyojin <wkdgywls30@gmail.com>
2022-12-25 18:05:32 +0900

    gradle 설정 변경

--- src/main/java/com/example/banktoyproject/elasticsearch/model/Subway.java
+++ src/main/java/com/example/banktoyproject/elasticsearch/model/Subway.java
@@ -1,17 +1,24 @@
 package com.example.banktoyproject.elasticsearch.model;
 
-import lombok.*;
+import com.opencsv.bean.CsvBindByName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
 public class Subway {
+    @CsvBindByName
     private String code;
+    @CsvBindByName
     private String station;
-    private String chosung;
-    private String jamo;
-    private String engtokor;
+    @CsvBindByName
     private String line;
+    @CsvBindByName
     private String excode;
-
+//
+//    private String chosung;
+//    private String jamo;
+//    private String engtokor;
 }
